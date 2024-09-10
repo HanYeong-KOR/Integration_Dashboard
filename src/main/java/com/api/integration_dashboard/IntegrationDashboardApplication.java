5ce260fbbea561a0b0effafd8d88cf17095449c9
Hanyeong Choi <younghy96@naver.com>
2024-09-10 11:13:02 +0900

    Initial commit

---
+++ src/main/java/com/api/integration_dashboard/IntegrationDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.api.integration_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationDashboardApplication.class, args);
+	}
+
+}
