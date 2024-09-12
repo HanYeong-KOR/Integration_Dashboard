09098827ba27865774f86a50423d4e8b321c805a
Hanyeong Choi <younghy96@naver.com>
2024-09-12 16:37:43 +0900

    240912 commit

--- src/main/frontend/src/services/ApiService.js
+++ src/main/frontend/src/services/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://localhost:9090/api";  // Spring Boot 백엔드 주소
+const API_BASE_URL = "http://localhost:9090/api";
 
 const fetchApiData = () => {
     return axios.get(`${API_BASE_URL}/fetch`);
